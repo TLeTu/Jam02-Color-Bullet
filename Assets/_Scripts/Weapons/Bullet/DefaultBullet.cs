24134dc4dc9572a88083bc39f1d77fca9651683b
Tu Tran Le <tranletu2003@gmail.com>
2024-10-30 15:13:49 +0700

    Fix red bullet

---
+++ Assets/_Scripts/Weapons/Bullet/DefaultBullet.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+
+public class DefaultBullet : Bullet
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private GrenadeLauncherBulletDmgDealer _dealer;
+
+    [SerializeField] private float _flySpeed = 1;
+    [SerializeField] private AnimationCurve _animationCurve;
+
+    [SerializeField] private float _damage;
+
+    private Vector2 _startPoint;
+    private Vector2 _aimPoint;
+    private bool _isFlying;
+    private float _flyTime;
+
+    private float _currentCurve;
+
+    private static readonly int FlyAnimation = Animator.StringToHash("Fly");
+    private static readonly int ExplodeAnimation = Animator.StringToHash("Explode");
+
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _isFlying = false;
+        _currentCurve = 0;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (_isFlying)
+        {
+            HandleFlying();
+        }
+    }
+
+    public override void Initialize(Weapon sourceWeapon)
+    {
+        transform.position = sourceWeapon.transform.position;
+        transform.rotation = sourceWeapon.transform.rotation;
+
+        _animator.CrossFade(FlyAnimation, 0, 0);
+
+
+    }
+
+    public void Firing(Vector2 aimPoint)
+    {
+        _startPoint = (Vector2)transform.position;
+        _aimPoint = aimPoint;
+        _isFlying = true;
+        _flyTime = Vector2.Distance(_startPoint, _aimPoint) / _flySpeed;
+
+        RotateToAimPoint(aimPoint);
+    }
+
+    private void RotateToAimPoint(Vector2 aimPoint)
+    {
+        Vector2 direction = aimPoint - (Vector2)transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void HandleFlying()
+    {
+        _currentCurve += Mathf.MoveTowards(0, 1, Time.deltaTime / _flyTime);
+
+        Vector2 point = Vector2.Lerp(_startPoint, _aimPoint, _animationCurve.Evaluate(_currentCurve));
+
+        transform.position = (Vector3)point;
+
+        if (_currentCurve >= 1)
+        {
+            Explode();
+            _isFlying = false;
+        }
+
+    }
+
+    private void Explode()
+    {
+        _animator.CrossFade(ExplodeAnimation, 0, 0);
+
+
+        _dealer.DealOneShotDamage(_damage);
+
+        StartDespawnTimer();
+    }
+}
